111059ec1799a4da3f893d304c0d18f119e083ce
Klevie Jun Caseres <klevmailbox04@gmail.com>
2022-06-07 20:17:32 +0800

    Movie JS From App.js to GlobalFunction.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,3 @@
-function upperCaseF(a){
-  setTimeout(function(){
-      a.value = a.value.toUpperCase();
-  }, 1);
-}
-
-
-function generateDateRangeLink(startDate, endDate){
- // startDate =  $('#startDate').datepicker({ dateFormat: 'yy-mm-dd' }).val();
-//  endDate =  $('#endDate').datepicker({ dateFormat: 'yy-mm-dd' }).val();
-
-  var sdate = startDate.split("/");
-  startDate = sdate[2] + "-" + sdate[0] + "-" + sdate[1];
-
-  var edate = endDate.split("/");
-  endDate = edate[2] + "-" + edate[0] + "-" + edate[1];
-
-  var link = "/" +  startDate + "/" + endDate;
-  
- // alert(link);
-  return link;
-  //$('#startDate').datepicker({ dateFormat: 'yy-mm-dd' }).datepicker('setDate', startDate);
-}
-
-function resetFilterTable(){
-  $('#startDate').val("");
-  $('#endDate').val("");
-}
 
 
 
